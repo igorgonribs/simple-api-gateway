35b4b71fb4b31df72676e0027633af644c34fb3c
igorgon <igorgonribsilva@gmail.com>
2021-04-04 10:58:19 -0300

    First local commit

---
+++ service-two/src/main/java/com/patterns/architecture/servicetwo/ServiceTwoApplication.java
@@ -0,0 +1,13 @@
+package com.patterns.architecture.servicetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTwoApplication.class, args);
+	}
+
+}
---
+++ service-two/src/test/java/com/patterns/architecture/servicetwo/ServiceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patterns.architecture.servicetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
